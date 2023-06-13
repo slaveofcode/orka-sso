b31a4931175e75d43b2be4b9d17a63ff8b470a13
Aditya Kresna <slaveofcode@gmail.com>
2023-06-13 22:29:55 +0700

    add session middleware

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 require('dotenv').config()
 const express = require('express');
 const passport = require('./passport');
+const session = require('express-session')
 
 const app = new express();
+
+const sessConfig = {
+  secret: process.env.SESSION_SECRET_KEY || 'im a flying panda',
+  cookie: {}
+}
+
+if (process.env.MODE === 'production') {
+  app.disable('x-powered-by')
+  app.enable('trust proxy');
+  sessConfig.cookie.secure = true;
+}
 
 app.use(express.urlencoded({
   extended: true,
 }));
 app.use(express.json({ limit: '15mb' }));
+app.use(session(sessConfig))
 app.use(passport.initialize());
 //app.use(passport.session());
 
